61185e597906f4077f80252dadce3388f5158846
him-kali <bwires947@gmail.com>
2025-02-08 22:39:31 +0300

    before populating the audio lubrary

--- src/test.py
+++ src/test.py
@@ -2,19 +2,20 @@ import requests
 import os
 import vlc
 import time
+import threading
 
 class SurahMedia:
     def __init__(self):
         self.base_url = "https://api.quran.com/api/v4"
         self.media_path = os.path.join("media","surahs")
         self.surah_dict = {}
-        self.reciter_dict = [{}]
+        self.reciter_dict = {}
         if not os.path.exists("media"):
             os.makedirs("media")
             print("media folder created")
         else:
             print("media folder already exists")
-        #self.get_surah_list()
+        self.get_surah_list()
         
     def get_surah_list(self):
         chapter_url = f"{self.base_url}/chapters"
@@ -41,8 +42,8 @@ class SurahMedia:
                     rid = reciter.get("id")
                     name = reciter.get("reciter_name")
                     if rid and name:
-                        self.temp_dict[rid] = name
-                        self.reciter_dict.append(self.temp_dict)
+                        self.reciter_dict[rid] = name
+                        #self.reciter_dict.append(self.temp_dict)
                 print("Reciter list updated.")
                 with open (os.path.join("media","reciter_list.txt"), "w") as f:
                     for reciter in self.reciter_dict:
@@ -55,7 +56,8 @@ class SurahMedia:
             with open (os.path.join("media","reciter_list.txt"), "r") as f :
                 for line in f:
                     rid, name = line.strip().split(maxsplit=1)
-                    self.reciter_dict.append({rid: name})
+                    self.reciter_dict[rid] = name
+                    #self.reciter_dict.append({rid: name})
             pass
     def get_surah_audio(self,chapter_id,recitation_id):
         recitation_url = f"{self.base_url}/chapter_recitations/{recitation_id}/{chapter_id}"
@@ -66,18 +68,20 @@ class SurahMedia:
             audio_url = data["audio_file"]["audio_url"]
             audio_response = requests.get(audio_url)
             if audio_response.status_code == 200:
-                with open(self.get_audio_path(chapter_id), "wb") as f:
+                with open(self.get_audio_path(chapter_id,recitation_id), "wb") as f:
                     f.write(audio_response.content)
             else:
                 print(f"Failed to download audio file: {audio_response.status_code}")
         else:
             print(f"Request failed: {response.status_code}")
-    def get_audio_path(self, chapter_id):
+        print(f"Audio file for {self.surah_dict[chapter_id]} downloaded")
+    def get_audio_path(self, chapter_id,reciter_id):
         folder_path = os.path.join(self.media_path, str(chapter_id))
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
-        file_name = f"{chapter_id} - {self.surah_dict[chapter_id]}-audio.mp3"
+        file_name = f"{chapter_id} - {self.surah_dict[chapter_id]} by {self.reciter_dict[str(reciter_id)]}audio.mp3"
         return os.path.join(folder_path, file_name)
+
     def play_surah_audio(self,chapter_id):
         if not os.path.exists(self.get_audio_path(chapter_id)):
             print("Audio file not found, downloading...")
@@ -99,8 +103,19 @@ class SurahMedia:
 
 test = SurahMedia()
 test.get_reciter_list()
-print(test.reciter_dict)
-#test.get_surah_audio(114,7)
+
+#print(test.reciter_dict['2'])#.get('7'))
+#print(test.reciter_dict)
+
+threads = []
+for i in range(1, 115):
+    t = threading.Thread(target=test.get_surah_audio, args=(i, 7))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+test.get_surah_audio(114,7)
 #test.play_surah_audio(114)
 
 
